d4154597ce7d436e9abfe436262c6fce6f1e7c9e
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-15 23:03:10 +0100

    feat: finds if person is Mancunian

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -5,7 +5,7 @@ export function getFillings(sandwich) {
 
 export function isFromManchester(person) {
 	if (person === undefined) throw new Error('person is required');
-	// Your code here!
+	return person.city.toLowerCase() == 'manchester';
 }
 
 export function getBusNumbers(people) {
