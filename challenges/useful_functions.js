51caee8ae4e55faec1dba405804a448af2ad6320
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-16 15:17:33 +0100

    add file functions are stored in

---
+++ challenges/useful_functions.js
@@ -0,0 +1,11 @@
+export function nthInitialCapital(word, n = 1) {
+	// returns first letter and capitalises it
+	// 'hello' => 'H' 
+	return word.charAt(n - 1).toUpperCase();
+}
+
+export function decimalPlace(num, dec = 2) {
+	// changes decimal places on a number
+	// 3.141592 => 3.14
+	return parseFloat(num.toFixed(dec));
+}
