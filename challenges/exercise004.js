f68f75de4f0faf3237097fe5e7661f409a7d53e0
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-16 01:29:05 +0100

    feat: can find a name beginning with certain letters

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,15 +1,22 @@
+import {
+	nthInitialCapital,
+} from './exercise001.js';
+
 export function findSmallNums(nums) {
 	if (!nums) throw new Error('nums is required');
 	
 	return nums.filter(num => {
 		return typeof(num) == 'number' && num < 1;
-	})
+	});
 }
 
 export function findNamesBeginningWith(names, char) {
 	if (!names) throw new Error('names is required');
 	if (!char) throw new Error('char is required');
-	// Your code here
+	
+	return names.filter(name => {
+		return nthInitialCapital(name) == char.toUpperCase();
+	});
 }
 
 export function findVerbs(words) {
