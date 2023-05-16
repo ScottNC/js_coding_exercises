626b683cbd0b0cc80ff8f248d69190e6dcd7152e
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-16 09:45:09 +0100

    feat: filters sentences on what word they have

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -55,7 +55,11 @@ export function getSquareRoots(nums) {
 export function findSentencesContaining(sentences, str) {
 	if (!sentences) throw new Error('sentences is required');
 	if (!str) throw new Error('str is required');
-	// Your code here
+	
+	return sentences.filter(sentence => {
+		return typeof(sentence) == 'string' && 
+			sentence.toLowerCase().includes(str.toLowerCase());
+	});
 }
 
 export function getLongestSides(triangles) {
