c9601dd0cfa1c6076b783459a3418496118094d2
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-17 20:05:06 +0100

    fix: includes check for input types in exercise 3

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -17,6 +17,7 @@ function concatItems (obj, key, filterFunc) {
 
 export function getSquares(nums) {
 	if (nums === undefined) throw new Error('nums is required');
+	if (!Array.isArray(nums)) throw new Error('nums must be array');
 
 	// if number, item is replaced by square of number
 	return nums.map(num => {
@@ -26,6 +27,7 @@ export function getSquares(nums) {
 
 export function camelCaseWords(words) {
 	if (words === undefined) throw new Error('words is required');
+	if (!Array.isArray(words)) throw new Error('words must be array');
 	
 	// if the word isn't first in the list, it is capitalised
 	return words.map((word, idx) => {
@@ -36,6 +38,7 @@ export function camelCaseWords(words) {
 
 export function getTotalSubjects(people) {
 	if (people === undefined) throw new Error('people is required');
+	if (!Array.isArray(people)) throw new Error('people must be array');
 	
 	let subjects = concatItems(people, 'subjects', item => typeof(item) === 'string')
 	return subjects.length;
@@ -44,6 +47,8 @@ export function getTotalSubjects(people) {
 export function checkIngredients(menu, ingredient) {
 	if (menu === undefined) throw new Error('menu is required');
 	if (!ingredient) throw new Error('ingredient is required');
+	if (!Array.isArray(menu)) throw new Error('menu must be array');
+	if (typeof(ingredient) !== 'string') throw new Error('ingredient must be string');
 
 	// Creates array of all times ingredient is used
 	let relevantIngredients = concatItems(menu, 'ingredients', item => item === ingredient);
@@ -54,7 +59,8 @@ export function checkIngredients(menu, ingredient) {
 export function duplicateNumbers(arr1, arr2) {
 	if (arr1 === undefined) throw new Error('arr1 is required');
 	if (arr2 === undefined) throw new Error('arr2 is required');
-	
+	if (!Array.isArray(arr1)) throw new Error('arr1 must be array');
+	if (!Array.isArray(arr2)) throw new Error('arr2 must be array');
 	// remove duplicate values from initial array as they are irrelevant
 	arr1 = [...new Set(arr1)];
 	arr2 = [...new Set(arr2)];
