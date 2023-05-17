6ec3f27ae5d47b691c110a3f2c2abc8078b86f37
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-17 01:13:36 +0100

    feat: finds if number is prime

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -56,6 +56,29 @@ export const getComplementaryDNA = (str) => {
  */
 export const isItPrime = (n) => {
 	if (n === undefined) throw new Error('n is required');
+
+	if (n === 2 || n === 3) return true;
+	if (n === 1 || !(n % 2) || !(n % 3)) return false;
+
+	// every prime number greater than 3 must either be 6n+1 or 6n-1
+	// by checking if it isn't either we can find if it isn't a prime and prevent having to iterate
+	// the code will still work by removing this it just saves coding power
+	if (((n - 1) % 6) && ((n + 1) % 6)) 
+		return false;
+
+	let iteration = 1;
+
+	// this iterates through every multiple of 6
+	// if n is divisible by potential primes greater than 3 (6n±1) then we know it's not prime
+	// if we iterate past the square root of n, n/factor must be smaller than the factor so the code will have passed it
+	// we can iterate through every number but this requires less iterations (we avoid checking multiples of 2 and 3 as it would be pointless)
+	while (6 * iteration - 1 <= n**0.5) {
+		if (!((n % (6 * iteration - 1)) && (n % (6 * iteration + 1)))) 
+			return false;
+		iteration++;
+	}
+
+	return true;
 };
 
 /**
