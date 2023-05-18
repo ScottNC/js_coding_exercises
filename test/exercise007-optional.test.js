51f3a7ac9b61b9364366a5060ef68432befddd3a
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-18 12:47:49 +0100

    test: adds test for summing digits

---
+++ test/exercise007-optional.test.js
@@ -0,0 +1,18 @@
+import {
+    sumDigits
+} from "../challenges/exercise007-optional";
+
+// import {
+//     simpleTypeTest,
+//     testMultipleArgs
+// } from "./test_functions.js";
+
+
+describe("sumDigits", () => {
+    test("returns sum of digits for integers", () => {
+        expect(sumDigits(1234)).toBe(10);
+        expect(sumDigits(1023)).toBe(6);
+        expect(sumDigits(0)).toBe(0);
+        expect(sumDigits(267836282)).toBe(44);
+    });
+});
