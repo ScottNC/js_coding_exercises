2e8a5d2510b4ec34d58efe9571064d6c762c3eb8
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-18 15:02:20 +0100

    test: tests createRange for integers

--- test/exercise007-optional.test.js
+++ test/exercise007-optional.test.js
@@ -22,3 +22,20 @@ describe("sumDigits", () => {
 
     simpleTypeTest(sumDigits, 'number', 'n');
 });
+
+describe("createRange", () => {
+    test("returns sum of digits for integers", () => {
+        expect(createRange(3, 11, 2)).toEqual([3, 5, 7, 9, 11]);
+        expect(createRange(2, 18, 2)).toEqual([2, 4, 6, 8, 10, 12, 14, 16, 18]);
+    });
+
+    const out = [12, 49, 86, 123, 160, 197, 234, 271, 308, 345, 382, 419,
+        456, 493, 530, 567, 604, 641, 678, 715, 752, 789, 826, 863, 900, 
+        937, 974, 1011, 1048, 1085, 1122, 1159, 1196, 1233, 1270, 1307, 
+        1344,1381, 1418, 1455, 1492, 1529, 1566, 1603, 1640, 1677, 1714, 
+        1751, 1788, 1825, 1862];
+
+    test("returns large array", () => {
+        expect(createRange(12, 1862, 37)).toEqual(out);
+    });
+});
