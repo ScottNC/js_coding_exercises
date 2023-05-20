cf16be846b225cb2f2f11d9bf4f898b12597ece2
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-05-20 19:02:58 +0100

    reformats parameter type tests

--- test/test_functions.js
+++ test/test_functions.js
@@ -1,19 +1,19 @@
-const typeValueExamples = {
+const TYPE_VALUE_EXAMPLES = {
     string : 'hello',
     number : 43,
     array : [ 1, 2, 3 ],
     object : { a : 1 },
-    boolean : false,
+    boolean : true,
     null: null,
     undefined: undefined
 };
 
 const findWrongInputType = (argument) => {
-    return Object.entries(typeValueExamples).find(([, val]) => val === argument)[0];
+    return Object.entries(TYPE_VALUE_EXAMPLES).find(([, val]) => val === argument)[0];
 };
 
 const getOtherTypes = (type) => {
-    return Object.entries(typeValueExamples).reduce((accumulator, [key, value]) => {
+    return Object.entries(TYPE_VALUE_EXAMPLES).reduce((accumulator, [key, value]) => {
         if (key !== type) 
             accumulator[accumulator.length] = value;
         return accumulator;
@@ -22,17 +22,20 @@ const getOtherTypes = (type) => {
 
 const testInputType = (func, type, argName, listArgs, argIsArray, wrongInputType, testFunc)  => {
     listArgs.forEach( arg => {
-        if (argIsArray) wrongInputType = findWrongInputType(arg);
+        // argIsArray is useful for when the input argument is itself an array so it doesn't use the values in the array as different inputs
+        if (argIsArray) {
+            wrongInputType = findWrongInputType(arg);
+            arg = [arg];
+        }
 
         testFunc("throws error for non " + type + " argument for input variable " + argName + " when inputting " + wrongInputType, () => {
-            // argIsArray is useful for when the input argument is itself an array so it doesn't use the values in the array as different inputs
-            arg = argIsArray? [arg] : arg;
             expect(() =>  func(...arg)).toThrow(Error)
         });
     });
 };
 
 // simple test to test if all other input types will return errors for functions with one input parameter
+// if you only want to test the input parameters input test.only as a final argument
 export const simpleTypeTest = (func, type, argName, testFunc = test) => {
     const otherTypes = getOtherTypes(type);
     testInputType(func, type, argName, otherTypes, true, null, testFunc);
@@ -42,7 +45,7 @@ export const simpleTypeTest = (func, type, argName, testFunc = test) => {
 export const testMultipleArgs = (func, allArgs, testFunc = test) => {
 
     // The template will allow the code to pass in the arguments not being tested correctly
-    const argsTemplate = allArgs.map(arg => typeValueExamples[arg.type]);
+    const argsTemplate = allArgs.map(arg => TYPE_VALUE_EXAMPLES[arg.type]);
 
     allArgs.forEach((arg, argOrder) => {
         // if argument can be any variable we can choose to skip the test but still have an input
